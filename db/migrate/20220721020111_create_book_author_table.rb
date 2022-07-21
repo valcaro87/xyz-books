30fade07bbf012275d6165bfc1fd3a1e1819ec39
valcaro87@gmail.com <valcaro87@gmail.com>
2022-07-21 22:46:44 +0800

    if table exist dont do anyting

--- db/migrate/20220721020111_create_book_author_table.rb
+++ db/migrate/20220721020111_create_book_author_table.rb
@@ -1,12 +1,14 @@
 class CreateBookAuthorTable < ActiveRecord::Migration[6.1]
   def change
-    create_table :book_authors do |t|
-      # t.integer :book_id
-      # t.integer :author_id
-      t.belongs_to :book
-      t.belongs_to :author
+    if !table_exists?(:book_authors)
+      create_table :book_authors do |t|
+        # t.integer :book_id
+        # t.integer :author_id
+        t.belongs_to :book
+        t.belongs_to :author
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
