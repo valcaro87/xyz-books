f58099cbb77e5b6c2f8f339a5d2293b93b2aae62
valcaro87@gmail.com <valcaro87@gmail.com>
2022-07-21 22:43:17 +0800

    migration file has the same name with model name

---
+++ db/migrate/20220721020111_create_book_author_table.rb
@@ -0,0 +1,12 @@
+class CreateBookAuthorTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :book_authors do |t|
+      # t.integer :book_id
+      # t.integer :author_id
+      t.belongs_to :book
+      t.belongs_to :author
+
+      t.timestamps
+    end
+  end
+end
