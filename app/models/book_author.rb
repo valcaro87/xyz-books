c533f2b87b5b813b1b7342a2e2e0275fb6a4fde5
valcaro87@gmail.com <valcaro87@gmail.com>
2022-07-21 22:37:56 +0800

    fixed migration

--- app/models/book_author.rb
+++ app/models/book_author.rb
@@ -1,4 +1,4 @@
 class BookAuthor < ApplicationRecord
-  belongs_to :book
-  belongs_to :author
+  # belongs_to :book
+  # belongs_to :author
 end
--- db/migrate/20220721020111_book_author.rb
+++ db/migrate/20220721020111_book_author.rb
@@ -1,10 +1,10 @@
 class BookAuthor < ActiveRecord::Migration[6.1]
   def change
     create_table :book_authors do |t|
-      # t.integer :book_id
-      # t.integer :author_id
-      t.belongs_to :book
-      t.belongs_to :author
+      t.integer :book_id
+      t.integer :author_id
+      # t.belongs_to :book
+      # t.belongs_to :author
 
       t.timestamps
     end
